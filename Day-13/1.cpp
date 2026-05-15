9c634ef8d0fc565e2ec7af9bbfed11817f7a7dc7
Gayatri Bhardwaj <goribhardwajgb@gmail.com>
2026-05-15 14:22:30 +0530

    normal commit

---
+++ Day-13/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fun(int x) {
+    if (x ==0) {
+        return 0;
+    }
+    return (x%10) + (x/10);
+}
+
+int main () {
+    int n;
+    cin >> n;
+
+    cout << fun(n) << endl;
+    return 0;
+}
